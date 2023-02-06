b0be0415a174cb68d93ef79cb6e14e7f2bada7f9
alegofrenicht <lalala@poopsex.com>
2023-02-06 05:14:39 +0300

    adding more features

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 let flag = true
+let src_flag = true
+const src_on = ["https://www.youtube.com/embed/DASLDeEIq44?&autoplay=1", "images/Speaker_Icon.svg"]
+const src_off = ["https://www.youtube.com/embed/DASLDeEIq44?&autoplay=1&mute=1", "images/volume-mute-icon.webp"]
+let volume = document.getElementById('volume')
+let sound = document.getElementById('sound')
+let icon = document.getElementById('icon')
+volume.addEventListener('click', function (){
+    if (src_flag){
+        console.log('pass')
+        sound.src = src_off[0]
+        icon.src = src_off[1]
+        src_flag = false
+    } else {
+        console.log('not')
+        sound.src = src_on[0]
+        icon.src = src_on[1]
+        src_flag = true
+    }
+
+})
+volume.addEventListener('mouseover', function (){
+    volume.style.cursor = 'pointer'
+})
+
+
 let patrick = document.getElementById('patrick')
 let quote_cloud = document.getElementById('quote-cloud')
 let size = 100
-// let yesNo_flag = true
+
+function quoteleave(){
+    let some = document.getElementById('quote')
+    some.style.cursor = 'default'
+}
+
+function quotover(){
+    let some = document.getElementById('quote')
+    some.style.cursor = 'pointer'
+}
 function quote_cloud_content(){
     let image = document.createElement('img')
     image.setAttribute('id', 'cloud')
@@ -11,10 +45,8 @@ function quote_cloud_content(){
     figcaption.setAttribute('id', 'quote')
     quote_cloud.append(image, figcaption)
     let quote = document.getElementById('quote')
-    quote.addEventListener('mouseover', function (){
-        quote.style.cursor = 'pointer'})
-    quote.addEventListener('mouseleave', function (){
-        quote.style.cursor = 'auto'})
+    quote.addEventListener('mouseover', quotover)
+    quote.addEventListener('mouseleave', quoteleave)
 }
 
 function patrickover(){
@@ -66,6 +98,8 @@ function move(){
                     let text = 'Leedle Leedle Leedle Lee... Hello, it\'s Patrick'
                     let quote_animation = setInterval(function (){
                         appear_cloud(element, text, quote_animation)
+                        patrick.removeEventListener('click', move)
+                        patrick.style.cursor = 'auto'
                         }, 80)
                 }
             }, 10)
@@ -104,10 +138,22 @@ function vanish(interval_func) {
         let text = 'Spoooooonge Boooob !!!'
         let interval_2 = setInterval(function (){
             appear_cloud(element, text, interval_2)
+            element.addEventListener('click', new_quotation)
         }, 80)
     }
 }
 
+function new_quotation(){
+    let quote_animation_1 = setInterval(function () {
+        distance--
+        quote_cloud.style.opacity = `${distance * 0.1}`
+        if (distance < 1) {
+            clearInterval(quote_animation_1)
+            spongebob()
+
+        }
+    }, 80)
+}
 
 function quotation (){
     let quote_animation_1 = setInterval(function (){
@@ -141,68 +187,145 @@ function add_buttons(){
     quote.removeEventListener('click', quotation)
     let yes = document.getElementById('yes')
     let no = document.getElementById('no')
+
     yes.addEventListener('click', function () {
-        let text = 'Spooooonge Boooob !!!'
+
         let interval = setInterval(function () {
             vanish(interval)
-            console.log(distance)
         }, 80)
     })
+
+
     no.addEventListener('click', function () {
         let interval = setInterval(function () {
             distance --
             quote_cloud.style.opacity = `${distance * 0.1}`
             if (distance < 1){
                 clearInterval(interval)
+                let patrick_anim = setInterval(function (){
+                    distance ++
+                    patrick.style.left = `-${distance}px`
+                    if (distance > 600){
+                        clearInterval(patrick_anim)
+                    }
+                }, 10)
             }
-        }, 80)
+        }, 60)
     })
 
 }
 
+function spongebob (){
+    let quote = document.getElementById('quote')
+    let img = document.createElement('img')
+    img.setAttribute('id', 'sponge')
+    img.setAttribute('src', 'images/SpongeBob.png')
+    document.body.append(img)
+    let sponge = document.getElementById('sponge')
+    sponge.addEventListener('mouseover', spongeover)
+    sponge.addEventListener('mouseleave', spongeleave)
+    sponge.addEventListener('click', sponganimation)
+    let appear = setInterval(function (){
+        distance ++
+        sponge.style.opacity = `${distance * 0.1}`
+        if (distance > 9){
+            clearInterval(appear)
+            quote.removeEventListener('mouseover', quotover)
+            quote.removeEventListener('click', new_quotation)
+        }
+    }, 150)
+}
 
-    // let interval = setInterval(function (){
-    //
-    // })
-    //         yes.addEventListener('click', function (){
-    //
-    //     console.log('yes')
-    //         let button_algorithm = setInterval(function (){
-    //             distance_new --
-    //             console.log(distance_new)
-    //             quote_cloud.style.opacity = `${distance_new * 0.1}`
-    //             if (distance_new < 1){
-    //                 clearInterval(button_algorithm)
-    //                 yesNo_flag = 20
-    //             }
-    //     }, 80)
-    //
-    // })
-    //     }
-    // }, 80)
+
+function spongeover(){
+    let sponge = document.getElementById('sponge')
+    sponge.style.cursor = 'pointer'
+    let animation = setInterval(function (){
+        size ++
+        sponge.style.height = `${size}px`
+        if (size > 120){
+            clearInterval(animation)
+        }
+    }, 1)
+}
+
+function spongeleave(){
+    let sponge = document.getElementById('sponge')
+    sponge.style.height = '100px'
+    size = 100
+}
 
 
-// figcaption.removeEventListener('click', quotation, false)
+function sponganimation(){
+    distance = 0
+    let position_sponge = 580 + 357
+    let sponge = document.getElementById('sponge')
+    sponge.removeEventListener('mouseover', spongeover)
+    sponge.removeEventListener('mouseleave', spongeleave)
+    sponge.style.cursor = 'default'
+    let interval = setInterval(function (){
+        distance ++
+        sponge.style.top = `${position_top + distance}px`
+        if (distance > 357){
+            clearInterval(interval)
+            distance = 0
+            sponge.style.height = '700px'
+            sponge.style.left = '550px'
+            sponge.src = 'images/Spongebob_Squarepants.webp'
+            let greeting = document.createElement('audio')
+            greeting.src = 'images/im-ready.mp3'
+            greeting.setAttribute('autoplay', '')
+            document.body.append(greeting)
+            let new_inter = setInterval(function (){
+                distance ++
+                sponge.style.top = `${position_sponge - distance}px`
+                if (distance > 700){
+                    clearInterval(new_inter)
+                    distance = 0
+                    let int = setInterval(function (){
+                        distance++
+                        let gary = document.getElementById('gary')
+                        gary.style.opacity = `${distance * 0.1}`
+                        let meow = document.createElement('audio')
+                        meow.src = 'images/gary_meow.mp3'
+                        meow.setAttribute('autoplay', '')
+                        meow.setAttribute('id', 'meow')
+                        document.body.append(meow)
+                        if (distance > 2){
+                            clearInterval(int)
+                            let gary_int = setInterval(function (){
+                                distance ++
+                                gary.style.opacity = `${distance * 0.1}`
+                                if (distance > 9){
+                                    clearInterval(gary_int)
+                                    gary.addEventListener('mouseover', garyover)
+                                    gary.addEventListener('mouseleave', garyleave)
+                                }
+                            }, 80)
+                        }
+                    }, 1000)
 
-//     no.addEventListener('click', function (){
-//         console.log('no')
-//           let button_algorithm = setInterval(function (){
-//             distance_new --
-//             quote_cloud.style.opacity = `${distance_new * 0.1}`
-//             if (distance_new < 1){
-//                 clearInterval(button_algorithm)
-//                 yesNo_flag = 10
-//
-//             }
-//         }, 80)
-//
-//     })
+                }
+            }, 10)
+        }
+    }, 20)
+}
 
-// let text = 'So'
-// let test = document.getElementById('test')
-// test.addEventListener('click', function (){testfunc(text)})
-// function testfunc(text){
-//     test.textContent = text
-// }
-//
-// text = 'No'
+function garyover(){
+    distance = 50
+    let gary = document.getElementById('gary')
+    gary.style.cursor = 'grab'
+    let animation = setInterval(function (){
+    distance ++
+    gary.style.height = `${distance}px`
+    if (distance > 70){
+        clearInterval(animation)
+        console.log('its done')
+    }
+    }, 1)
+}
+
+function garyleave(){
+    let gary = document.getElementById('gary')
+    gary.style.height = '50px'
+}
